61f355e63952bcd6424ef29c9f90d9a570262a14
Mark Tiedemann <www.marktiedemann@gmail.com>
2021-09-09 18:50:37 +0200

    Add balloon tooltip

--- src/Tray.ps1
+++ src/Tray.ps1
@@ -1,25 +1,40 @@
 [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms');
 [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing');
 
+$Url = 'http://localhost:8000/';
+
 $Process = [System.Diagnostics.Process]::New();
-$Process.StartInfo.FileName = 'powershell';
-$Process.StartInfo.Arguments = '-f Server.ps1';
-$Process.StartInfo.UseShellExecute = $false;
-$Process.StartInfo.CreateNoWindow = $true;
+$StartInfo = $Process.StartInfo;
+$StartInfo.FileName = 'powershell';
+$StartInfo.Arguments = '-f Server.ps1';
+$StartInfo.UseShellExecute = $false;
+$StartInfo.CreateNoWindow = $true;
 $Process.Start();
 
 $IconPath = [System.Environment]::ExpandEnvironmentVariables('%SystemRoot%\System32\newdev.exe');
 $Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($IconPath);
 
 $CurrentUICulture = [System.Globalization.CultureInfo]::CurrentUICulture;
-$Text = switch ($CurrentUICulture.TwoLetterISOLanguageName) {
+$Language = $CurrentUICulture.TwoLetterISOLanguageName;
+
+$TrayText = switch ($Language) {
 	'en' { 'Close server' }
-	'de' { 'Server schließen' }
+	'de' { 'Server beenden' }
+};
+$BalloonTitle = switch ($Language) {
+	'en' { 'Server listening on ' + $Url }
+	'de' { 'Server horcht auf ' + $Url }
+};
+$BalloonText = switch ($Language) {
+	'en' { 'Click here to open. To close, click tray icon.' }
+	'de' { 'Zum Aufrufen hier klicken. Zum Beenden Ablagebild anklicken.' }
 };
 
 $NotifyIcon = [System.Windows.Forms.NotifyIcon]::New();
 $NotifyIcon.Icon = $Icon;
-$NotifyIcon.Text = $Text;
+$NotifyIcon.Text = $TrayText;
+$NotifyIcon.BalloonTipTitle = $BalloonTitle;
+$NotifyIcon.BalloonTipText = $BalloonText;
 $NotifyIcon.Visible = $true;
 $NotifyIcon.Add_Click({
 	if (!$Process.HasExited) {
@@ -27,6 +42,10 @@ $NotifyIcon.Add_Click({
 	}
 	$ApplicationContext.ExitThread();
 });
+$NotifyIcon.Add_BalloonTipClicked({
+	rundll32.exe url.dll,FileProtocolHandler $Url;
+});
+$NotifyIcon.ShowBalloonTip(0);
 
 $ApplicationContext = [System.Windows.Forms.ApplicationContext]::New();
 [System.Windows.Forms.Application]::Run($ApplicationContext);
