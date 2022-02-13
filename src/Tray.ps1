7c5a69290ddcf2c808d1f178b064b47f9ce1bc4f
Mark Tiedemann <www.marktiedemann@gmail.com>
2022-02-13 18:02:12 +0100

    Remove balloon notification

--- src/Tray.ps1
+++ src/Tray.ps1
@@ -1,8 +1,6 @@
 [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms');
 [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing');
 
-$Url = 'http://localhost:__REPLACE_PORT__/';
-
 $Process = [System.Diagnostics.Process]::New();
 $StartInfo = $Process.StartInfo;
 $StartInfo.FileName = 'powershell';
@@ -11,14 +9,9 @@ $StartInfo.UseShellExecute = $false;
 $StartInfo.CreateNoWindow = $true;
 $Process.Start();
 
-$IconPath = [System.Environment]::ExpandEnvironmentVariables('%SystemRoot%\System32\newdev.exe');
-$Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($IconPath);
-
 $NotifyIcon = [System.Windows.Forms.NotifyIcon]::New();
-$NotifyIcon.Icon = $Icon;
+$NotifyIcon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon([System.Environment]::ExpandEnvironmentVariables('%SystemRoot%\System32\newdev.exe'));
 $NotifyIcon.Text = 'Close server';
-$NotifyIcon.BalloonTipTitle = 'Server listening on ' + $Url;
-$NotifyIcon.BalloonTipText = 'Click here to open. To close, click tray icon.';
 $NotifyIcon.Visible = $true;
 $NotifyIcon.Add_Click({
 	if (!$Process.HasExited) {
@@ -26,10 +19,6 @@ $NotifyIcon.Add_Click({
 	}
 	$ApplicationContext.ExitThread();
 });
-$NotifyIcon.Add_BalloonTipClicked({
-	rundll32.exe url.dll,FileProtocolHandler $Url;
-});
-$NotifyIcon.ShowBalloonTip(0);
 
 $ApplicationContext = [System.Windows.Forms.ApplicationContext]::New();
 [System.Windows.Forms.Application]::Run($ApplicationContext);
