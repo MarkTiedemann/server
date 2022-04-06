e40b3fac9fca894c23c9d3d4a035837fb3411f9b
Mark Tiedemann <www.marktiedemann@gmail.com>
2022-04-06 23:39:01 +0200

    Improve mime mapping

--- src/Server.ps1
+++ src/Server.ps1
@@ -4,6 +4,33 @@ $Server = [System.Net.HttpListener]::New();
 $Server.Prefixes.Add('http://localhost:__REPLACE_PORT__/');
 $Server.Start();
 
+function Get-ContentType($Pathname) {
+	$Extension = [System.IO.Path]::GetExtension($Pathname);
+	$OctetStream = 'application/octet-stream';
+	$ContentType = switch ($Extension) {
+		'.csv' { 'text/csv' }
+		'.jsonld' { 'application/ld+json' }
+		'.mjs' { 'text/javascript' }
+		'.oft' { 'font/otf' }
+		'.ttf' { 'font/ttf' }
+		'.wasm' { 'application/wasm' }
+		'.webp' { 'image/webp' }
+		'.woff' { 'font/woff' }
+		'.woff2' { 'font/woff2' }
+		default { $OctetStream }
+	};
+	if ($ContentType -eq $OctetStream) {
+		$ContentType = [System.Web.MimeMapping]::GetMimeMapping($Extension);
+		if ($ContentType -eq $OctetStream) {
+			$ContentType = [Microsoft.Win32.Registry]::GetValue('HKEY_CLASSES_ROOT\' + $Extension, 'Content Type', $null);
+			if ($ContentType -eq $null) {
+				$ContentType = $OctetStream;
+			}
+		}
+	}
+	return $ContentType;
+}
+
 while ($Server.IsListening) {
 	try {
 		$Context = $Server.GetContext();
@@ -15,7 +42,7 @@ while ($Server.IsListening) {
 		}
 		$Path = '__REPLACE_ROOT__' + $Pathname;
 		$File = [System.IO.File]::Open($Path, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite);
-		$Response.ContentType = [System.Web.MimeMapping]::GetMimeMapping($Path);
+		$Response.ContentType = Get-ContentType($Path);
 		$File.CopyTo($Response.OutputStream);
 	} catch {
 		$Response.StatusCode = 404;
